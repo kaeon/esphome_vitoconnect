d9c8bb8712a1dccc194f74aaee926b2d1b186b8d
Kevin De Smet <kevin@lucidity.be>
2025-10-31 22:35:55 +0100

    Fix path

--- components/vitoconnect/vitoconnect.cpp
+++ components/vitoconnect/vitoconnect.cpp
@@ -18,7 +18,7 @@
 */
 
 #include "vitoconnect.h"
-#include "./number/vitoconnect_number.h"
+#include "src/esphome/components/vitoconnect/number/vitoconnect_number.h"
 
 namespace esphome {
 namespace vitoconnect {
